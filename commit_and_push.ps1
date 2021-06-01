c871032e310e7d9b9860cb459e01b19330471a3b
Jake Hallam <jv2-hallam@uwe.ac.uk>
2021-06-01 10:22:53 +0100

    Updated pipelines

--- commit_and_push.ps1
+++ commit_and_push.ps1
@@ -3,3 +3,7 @@ git add .;
 git commit -m "Updated pipelines";
 git push;
 echo "Commit : $(git rev-parse head)";                                                                                                                                       
+for($i = 0; $i -lt 7; $i += 1){
+    echo "waiting : $i / 6";
+    sleep 1 
+}
